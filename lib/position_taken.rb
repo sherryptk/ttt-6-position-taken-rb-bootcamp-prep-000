089dd2d313d3101ff83959121c280325b42cc310
Sherry <sherryptk@gmail.com>
2017-10-07 17:06:42 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -5,7 +5,7 @@ def position_taken?(board, index)
     false
   elsif board[index]== nil
     false
-  elsif board[index]==""
+  elsif board[index]== ""
     false
   else board[index]== "X" || "O"
     true
